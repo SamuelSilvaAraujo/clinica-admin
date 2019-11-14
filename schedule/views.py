0541cda102a423bc4805a84c3db3710b0b6bf733
samuel <samuelsas.samuel18@gmail.com>
2019-11-14 09:44:25 -0300

    add urls file e testando com template

--- schedule/views.py
+++ schedule/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class ScheduleView(TemplateView):
+    template_name = 'schedule/schedule.html'
