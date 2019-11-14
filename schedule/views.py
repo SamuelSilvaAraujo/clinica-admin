3e7d5ef5479aa27b44b6a4c1e9c815f934672eac
samuel <samuelsas.samuel18@gmail.com>
2019-11-14 16:03:50 -0300

    add template

--- schedule/views.py
+++ schedule/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render
 from django.views.generic import TemplateView
 
 
 class ScheduleView(TemplateView):
-    template_name = 'schedule/schedule.html'
+    template_name = 'schedule.html'
