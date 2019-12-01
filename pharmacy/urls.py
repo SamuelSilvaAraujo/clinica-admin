a1de66bd165603f50b0c3379c629bc938bad76b8
Samuel Araujo <samuelsas.samuel18@gmail.com>
2019-12-01 20:19:44 -0300

    add lista de categorias e estoque

--- pharmacy/urls.py
+++ pharmacy/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 
-from .views import MedicineListView
+from .views import *
 
 urlpatterns = [
-    path('remedios/', MedicineListView.as_view(), name='remedios'),
+    path('remedios/', MedicineListView.as_view(), name='medicines'),
+
+    path('categorias/', MedicineCategoryListView.as_view(), name='categories'),
+
+    path('estoque/', StockListView.as_view(), name='estoque')
 ]
--- pharmacy/views.py
+++ pharmacy/views.py
@@ -1,7 +1,27 @@
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-from .models import Medicine
+from .models import Medicine, MedicineCategory, Lote
+
+
+class MedicineCategoryListView(LoginRequiredMixin, ListView):
+    model = MedicineCategory
+    template_name = 'medicine_category/list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(MedicineCategoryListView, self).get_context_data(**kwargs)
+        context["medicinePage"] = "active"
+        return context
+
+
+class StockListView(LoginRequiredMixin, ListView):
+    model = Lote
+    template_name = 'stock/list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(StockListView, self).get_context_data(**kwargs)
+        context["medicinePage"] = "active"
+        return context
 
 
 class MedicineListView(LoginRequiredMixin, ListView):
